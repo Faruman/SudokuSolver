2128e7bbf14376091d5e8138fa0987a1b1cace96
Faruman <fabian.karst@student.unisg.ch>
2020-04-27 12:00:43 +0200

    Developing WebInterface. Implemented desktop and mobile versions.

--- WebApp/static/custom.js
+++ WebApp/static/custom.js
@@ -1,24 +1,16 @@
 function adjustSudokuBox() {
     $('.sudoku-box').each(function() {
-            $(this).css('font-size', 0.3*parseFloat($(this).parent().css('width'))/9);
+            $(this).css('font-size', Math.max(0.3*parseFloat($(this).parent().css('width'))/9, 10));
             $(this).css('height', parseFloat($(this).parent().css('width'))/9)
         }
     )
 }
 
-function adjustButton(){
-    $(".input-btn").each(function(){
-        $(this).css('font-size', 0.3*parseFloat($('.sudoku').css('width'))/9)
-    })
-}
-
 $(document).ready(function() {
     adjustSudokuBox()
-    adjustButton()
 });
 
 $( window ).resize(function() {
     adjustSudokuBox()
-    adjustButton()
     //add function to add toggle classes to navbar header if screen size is below threshold
 });
