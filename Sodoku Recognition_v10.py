1d5059785cdb639cfff5a0d0205ba2582a748682
sddiener <55442757+sddiener@users.noreply.github.com>
2020-05-03 20:31:38 +0200

    Crop soduoku is more robust

--- Sodoku Recognition_v10.py
+++ Sodoku Recognition_v10.py
@@ -10,14 +10,27 @@ os.chdir("C:\\Users\\sdien\\Documents\\GitHub\\SudokuSolver")
 
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
-
 
 
 #%% Define functions
 
+def show(img):
+    # resize but keep aspect ratio
+    h,w = img.shape[0:2]
+    ratio = h/w        
+    
+    new_h = 500
+    new_w = int(new_h * ratio)
+    img = cv2.resize(img, (new_w, new_h))
+
+    # show img (hit enter to close window)
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 def load_imgdata():
-    global imgs, imgs_upload, imgs_test
+    global imgs, imgs_upload
     imgs = []
     imgs_upload = []
     path1 = "data\\sudoku_img\\mixed\\"
@@ -32,29 +45,30 @@ def load_imgdata():
 def extract_sudoku(img):
     # Detect edges with "Canny Edge Detector"
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) # convert to gray
-    img_edges = cv2.Canny(img_gray,30,150) # detects edges in image
+    img_blur = cv2.GaussianBlur(img_gray, (5,5), 1)
+    img_edges = cv2.Canny(img_blur,30,200) # detects edges in image
     
     # Find largest contour
     _, contours,_ = cv2.findContours(img_edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) # get contours
-    largest_cnt = sorted(contours, key=cv2.contourArea, reverse=True)[0] # sort by area and take first element
+    cnt = max(contours, key = cv2.contourArea) # find largest contour by area
+    cnt = cnt.reshape((-1,2))
 
-    # Draw largest rectangle
+    # Find corner points of contour
+    s = np.sum(cnt, axis=1)             # x + y
+    tl = tuple(cnt[np.argmin(s)])   # lower sum; top left
+    br = tuple(cnt[np.argmax(s)])   # higer sum; bottom right
+
+    diff = np.diff(cnt, axis=1)         # x - y
+    tr = tuple(cnt[np.argmin(diff)])    # lower diff; top right
+    bl = tuple(cnt[np.argmax(diff)])    # higer diff; bottom left
+   
+    """# Draw largest rectangle with corner poitns
     rect = np.zeros(img.shape[0:2], dtype=np.uint8)
-    rect = cv2.drawContours(rect, [largest_cnt], 0, (255), 2)
-    
-    # Find corners points with "Shi-Tomasi Corner Detector"
-    pts = cv2.goodFeaturesToTrack(rect, minDistance = 20 ,maxCorners = 4, qualityLevel=0.01)
-    pts = np.int0(pts)
-    pts = pts.reshape((4,2))
-
-    # Sort corner points
-    s = pts.sum(axis=1)             # x + y
-    tl = tuple(pts[np.argmin(s)])   # lower sum; top left
-    br = tuple(pts[np.argmax(s)])   # higer sum; bottom right
-
-    diff = np.diff(pts, axis=1)         # x - y
-    tr = tuple(pts[np.argmin(diff)])    # lower diff; top right
-    bl = tuple(pts[np.argmax(diff)])    # higer diff; bottom left
+    rect = cv2.drawContours(rect, [cnt], 0, (255), 2)
+    for (x,y) in [tl,tr,bl,br]:
+        rect = cv2.circle(rect, (x,y), 10, 255, thickness = -1)
+    show(rect)
+    """
    
     # Adjust perspective 
     rows = cols = 500
@@ -64,10 +78,10 @@ def extract_sudoku(img):
     
     M = cv2.getPerspectiveTransform(pts1,pts2)
     cimg = cv2.warpPerspective(img,M,(cols, rows))
-    
+
     return cimg
 
-    
+
 def extract_numbers(cimg):
     # Split image into 81 cells with numbers 
     nums = [] 
@@ -107,34 +121,29 @@ def list81_to_image(list81):
     return plot
     
         
-#%% TEST: on one image
-      
+#%% TEST
+
+# Test ONE   
 load_imgdata()
 
-img = imgs_upload[2]     # somewhat working example: imgs[1], img_uplaod[1,3]
-plt.imshow(img)
-
-
+img = imgs[54]     # somewhat working example: imgs[1], img_uplaod[1,3]
 cimg = extract_sudoku(img)
-plt.imshow(cimg)
-
-
+show(img)
+show(cimg)
+ 
 numbers_list = extract_numbers(cimg)
 numbers_plot = list81_to_image(numbers_list)
-plt.imshow(numbers_plot, 'gray')
+show(numbers_plot)
 
-
-
-   
+# Test MANY
+for img in imgs[50:]:      
+    cimg = extract_sudoku(img)
     
+    show(cimg)
+   
+# Crop-Probleme: 2, 8, 14, 38, 45, 47, 54
+
          
 #%% Other Stuff   
     
             
-        """# Filter based on area
-        areas = stats[:,-1] # extract area from stats
-        areas = np.where(areas == np.max(areas), 0, areas)  # set largest component (= background) to 0
-        areas = np.where(areas < 0.01 * np.sum(areas), 0, areas) # set components below 1% (= noise) to 0
-        idx = np.nonzero(areas)"""
-    
-    
